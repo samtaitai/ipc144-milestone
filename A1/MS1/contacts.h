e3e63744eef3f606eb39cf32c8faf14a0ad79c77
Cameron Gray <cameron.gray@senecacollege.ca>
2019-10-14 16:42:22 -0400

    Assignment 1 (MS1-2) Released

---
+++ A1/MS1/contacts.h
@@ -0,0 +1,25 @@
+//==============================================
+// Name:           Full name here
+// Student Number: #########
+// Email:          userID@myseneca.ca
+// Section:        XXX
+// Date:           
+//==============================================
+// Assignment:     1
+// Milestone:      1
+//==============================================
+
+// Structure type Name declaration
+struct Name {
+    char firstName[31];
+    char middleInitial[7];
+    char lastName[36];
+};
+
+// Structure type Address declaration
+// Place your code here...
+
+
+// Structure type Numbers declaration
+// Place your code here...
+
