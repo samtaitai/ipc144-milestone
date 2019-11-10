072a6c97849e0c75129eff521e7e80c60a4cf9b4
Cameron Gray <cameron.gray@senecacollege.ca>
2019-11-10 18:52:08 -0500

    Assignment 2 (MS3-4) Released

---
+++ A2/MS3/contactHelpers.h
@@ -0,0 +1,97 @@
+//==============================================
+// Name:           Full name here
+// Student Number: #########
+// Email:          userID@myseneca.ca
+// Section:        XXX
+// Date:           
+//==============================================
+// Assignment:     2
+// Milestone:      3
+//==============================================
+
+/*-------------------------------------------------------------------------
+Description: This header file modularizes general helper functions
+             to help reduce redundant coding for common repetitive tasks.
+------------------------------------------------------------------------- */
+
+// Hint:  This header file now has functions with parameters referring to 
+//        the struct Contact type so be sure to include the contacts.h header:
+
+
+//--------------------------------
+// Function Prototypes
+//--------------------------------
+
+// +-------------------------------------------------+
+// | NOTE:  Copy/Paste your Assignment-2 Milestone-2 |
+// |        function prototypes below...             |
+// +-------------------------------------------------+
+
+// Clear the standard input buffer
+// clearKeyboard:
+
+// pause:
+
+
+// getInt:
+
+
+// getIntInRange:
+
+
+// yes:
+
+
+// menu:
+
+
+// contactManagerSystem:
+
+
+// +-------------------------------------------------+
+// | ====== Assignment 2 | Milestone 3 =======       |
+// +-------------------------------------------------+
+// | NOTE:  Add new function prototypes as per the   |
+// |        Milestone-3 specifications document      |
+// +-------------------------------------------------+
+
+// Generic function to get a ten-digit phone number (provided for you)
+void getTenDigitPhone(char phoneNum[]);
+
+// findContactIndex (provided for you)
+int findContactIndex(const struct Contact contacts[], int size, const char cellNum[]);
+
+
+// displayContactHeader
+// put function prototype here:
+
+
+// displayContactFooter:
+// put function prototype here:
+
+
+// displayContact:
+// put function prototype here:
+
+
+// displayContacts:
+// put function prototype here:
+
+
+// searchContacts:
+// put function prototype here:
+
+
+// addContact:
+// put function prototype here:
+
+
+// updateContact:
+// put function prototype here:
+
+
+// deleteContact:
+// put function prototype here:
+
+// sortContacts:
+// put function prototype here:
