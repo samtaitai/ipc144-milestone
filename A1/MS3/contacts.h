8a9ae404ef17b42fa5c7ee03d9590450adc469ac
Rob Robson <robert.robson1@senecacollege.ca>
2020-03-01 18:24:41 -0500

    Release of AS1 MS 3 & 4

---
+++ A1/MS3/contacts.h
@@ -0,0 +1,28 @@
+//==============================================
+// Name:           Full name here
+// Student Number: #########
+// Email:          userID@myseneca.ca
+// Section:        XXX
+// Date:           
+//==============================================
+// Assignment:     1
+// Milestone:      3
+//==============================================
+
+// Structure type Name declaration
+struct Name {
+    char firstName[31];
+    char middleInitial[7];
+    char lastName[36];
+};
+
+// Structure type Address declaration
+// Place your code from Milestone #2 here...
+
+
+// Structure type Numbers declaration
+// Place your code from Milestone #2 here...
+
+
+// Structure type Contact declaration
+// Place your code here...
