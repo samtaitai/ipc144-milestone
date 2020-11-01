92917ca7f29f8e356b6cd2cbb1f6bbc3ccd5c9ca
Robert Robson <robert.robson1@senecacollege.ca>
2020-11-01 09:05:11 -0500

    Release for 2207.

---
+++ A2/MS1/contactHelpers.c
@@ -0,0 +1,62 @@
+//==============================================
+// Name:           Full name here
+// Student Number: #########
+// Email:          userID@myseneca.ca
+// Section:        XXX
+// Date:           
+//==============================================
+// Assignment:     2
+// Milestone:      1
+//==============================================
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+// This source file needs to "know about" the functions you prototyped
+//       in the contact helper header file.
+// HINT: Put the header file name in double quotes so the compiler knows
+//       to look for it in the same directory/folder as this source file
+//       #include your contactHelpers header file on the next line:
+
+
+
+//--------------------------------
+// Function Definitions
+//--------------------------------
+
+// +-------------------------------------------------+
+// | ====== Assignment 2 | Milestone 1 =======       |
+// +-------------------------------------------------+
+// | Put empty function definitions below...         |
+// |                                                 |
+// | - The clearKeyboard function is done for you    |
+// +-------------------------------------------------+
+
+// clearKeyboard:  Empty function definition 
+void clearKeyboard(void)
+{
+
+}
+
+// pause: Empty function definition goes here:
+
+
+
+// getInt: Empty function definition goes here:
+
+
+
+// getIntInRange: Empty function definition goes here:
+
+
+
+// yes: Empty function definition goes here:
+
+
+
+// menu: Empty function definition goes here:
+
+
+
+// contactManagerSystem: Empty function definition goes here:
