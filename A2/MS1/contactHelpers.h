e84efaac1ed8a5ccc3b15ca5ead0820595bf2cbc
Robert Robson <robert.robson1@senecacollege.ca>
2021-07-06 12:56:19 -0400

    Release of A2 MS 1 & MS2 for 2214.

---
+++ A2/MS1/contactHelpers.h
@@ -0,0 +1,50 @@
+//==============================================
+// Name:           Full name here
+// Student Number: #########
+// Email:          userID@myseneca.ca
+// Section:        XXX
+// Date:           
+//==============================================
+// Assignment:     2
+// Milestone:      1
+//==============================================
+
+/*--------------------------------------------------------------------
+Description: This header file modularizes general helper functions to
+             help reduce redundant coding for common repetitive tasks. 
+---------------------------------------------------------------------*/
+
+// HINT: 
+// You will most likely want to include this header file
+// in other files of the project since it provides general 
+// helper functions that can be used throughout.
+
+
+//--------------------------------
+// Function Prototypes
+//--------------------------------
+
+// +-------------------------------------------+
+// | ====== Assignment 2 | Milestone 1 ======= |
+// +-------------------------------------------+
+
+// Clear the standard input buffer
+void clearKeyboard(void);
+
+// pause:
+// Put function prototype below:
+
+// getInt:
+// Put function prototype below:
+
+// getIntInRange:
+// Put function prototype below:
+
+// yes:
+// Put function prototype below:
+
+// menu:
+// Put function prototype below:
+
+// contactManagerSystem:
+// Put function prototype below:
