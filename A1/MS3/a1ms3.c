32feef8f79a91058282c8f301a7e787f3b93817e
Cameron Gray <cameron.gray@senecacollege.ca>
2021-10-31 10:56:51 -0400

    A1:MS3-4 Released

---
+++ A1/MS3/a1ms3.c
@@ -0,0 +1,57 @@
+// #############################################################################################
+//  Assignment: 1 
+//  Milestone : 3
+// #############################################################################################
+//  Student Name  : 
+//  Student ID    : 
+//  Student Email :
+//  Course Section: 
+// #############################################################################################
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+#include "account.h"
+#include "accountTicketingUI.h"
+
+// ------------------------------------------------------------------
+// Main entry point to application:
+
+int main(void)
+{
+    // *** NOTE: No other variables are allowed to be declared ***
+    struct Account account = { 0 };                             // *** DO NOT MODIFY THIS LINE ***
+
+    printf("Assignment 1 Milestone 3 - Tester\n");              // *** DO NOT MODIFY THIS LINE ***
+    printf("=============================================\n");  // *** DO NOT MODIFY THIS LINE ***
+
+    // ---------------------------------------------------------
+    // Account Data
+    // ---------------------------------------------------------
+    // Input (new record)
+    //________________(__________________);   // Call function to get user input for a new "Account"
+
+    // ---------------------------------------------------------
+    // Person Data
+    // ---------------------------------------------------------
+    // Input (new record)
+    //________________(__________________);   // Call function to get user input for a new "Person"
+
+    // ---------------------------------------------------------
+    // UserLogin Data
+    // ---------------------------------------------------------
+    // Input (new record)
+    //________________(__________________);   // Call function to get user input for a new "UserLogin"
+
+    // ---------------------------------------------------------
+    // Display formatted Account details
+    // ---------------------------------------------------------
+    //_______________________________;         // Call function to display the account detail header
+    // ________________(__________________);   // Call function to display an account detail record
+
+    putchar('\n');
+    printf("Account, Person, and User Login test completed!\n");  // *** DO NOT MODIFY THIS LINE ***
+
+    return 0;       // *** DO NOT MODIFY THIS LINE ***
+}
--- A1/MS4/README.md
+++
@@ -1,3 +0,0 @@
-# Assignment-1
-
-Stay tuned... this will be posted later in the semester.
